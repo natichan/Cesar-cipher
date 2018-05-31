13446320546b6551f8d6b6206e9fc343e687210e
Natalia <natalia@natalia.localdomain>
2018-05-31 22:38:49 +0000

    Cifrado creado en cipher.js y index.js, modifación del README Y HTML

--- cipher.js
+++ cipher.js
@@ -1,14 +1,42 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-  },
-  decode: () => {
-    /* Acá va tu código */
+  encode: (message, llave) => { //convertir letra del abecedario en ascii code.
+    let textCipher = "";
+    //creo una variable para guardar el mensaje cifrado.
+    for (let index = 0; index < message.length; index++){ 
+    //recorro mensaje con un loop.
+        let asciiPosition = message.charCodeAt(index);
+      //charCodeAt me da el numero ascii del mensaje, y la guardo en una variable.
+        asciiPosition = ((asciiPosition - 65 + llave) %26 + 65);
+        //aplico formula para que me de el caracter. 
+        //Vamos a sacar el residuo de la posicion dentro del codigo ascii para llevarlo al codigo humano
+        let numberToString = String.fromCharCode (asciiPosition);
+        //Tomo el codigo ascii y me da una letra
+       if (asciiPosition >=65 && asciiPosition <= 90)
+      //fijo una condición para que solo se mueva en las letras mayusculas del ascci code.
+
+        textCipher += numberToString
+         }
+        return textCipher;
+      },
+  decode: (message, llave) => {
+
+    let textCipher = "";
+    //creo una variable para guardar el mensaje cifrado.
+    for (let index = 0; index < message.length; index++){ 
+    //recorro mensaje con un loop.
+        let asciiPosition = message.charCodeAt(index);
+      //charCodeAt me da el numero ascii del mensaje, y la guardo en una variable.
+        asciiPosition = ((asciiPosition + 65 - llave) %26 - 65);
+        //aplico formula para que me de el caracter. 
+        //Vamos a sacar el residuo de la posicion dentro del codigo ascii para llevarlo al codigo humano
+        let numberToString = String.fromCharCode (asciiPosition);
+        //Tomo el codigo ascii y me da una letra
+       if (asciiPosition >=65 && asciiPosition <= 90)
+      //fijo una condición para que solo se mueva en las letras mayusculas del ascci code.
+
+        textCipher += numberToString
+         }
+        return textCipher;
+      },
   }
-} 
-  /*Para inspirarme 
-  const abc= "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
-  for (let index = 0; index < abc.length; index++){
-      document.writeln("Letra" +
-      (index)+ "del abecedario : "+ abc.charAt(index));
-  }*/
+
--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-/* Acá va tu código */
+
+myFunction=()=> {
+//función botón cifrar
+    let mes = document.getElementById("message1").value;
+    //llamando al mensaje que escribe el usuario en HTML.
+    let key = parseInt(document.getElementsByTagName("input")[0].value);
+    //llamando a la llave en HTML que será el offset.
+    document.getElementById("result").innerHTML = 
+    //se escribe el resultado en el recuadro resultado.
+    window.cipher.encode(mes, key);
+    //llamo a la función encode.
+}
+myFunction2=()=> {
+//función botón descifrar
+    let mes = document.getElementById("message1").value;
+    let key = parseInt(document.getElementsByTagName("input")[0].value);
+    document.getElementById("result").innerHTML = 
+    window.cipher.decode(mes, key);
+    //llamo a la función decode.
+
+}
+    
